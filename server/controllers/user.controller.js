700db7273b9c9c7f4b2955b014b7d677e4c3e9fc
Saromazzotta <Saromazzotta@gmail.com>
2023-06-11 18:16:05 -0400

    added backend logic for login and registration

--- server/controllers/user.controller.js
+++ server/controllers/user.controller.js
@@ -1,20 +1,51 @@
 const User = require("../models/user.model");
+const jwt = require("jsonwebtoken");
+const secret = process.env.FIRST_SECRET_KEY;
+const bcrypt = require("bcrypt");
 
 module.exports = {
     register: async (req, res) => {
         try {
             const potentialUser = await User.findOne({ email: req.body.email });
             if (potentialUser) {
-                res.status(400).json({message: "Email already exists"})
+                res.status(400).json({ message: "Email already exists" })
             } else {
                 const newUser = await User.create(req.body);
-                res.json({ message: "succes", user: newUser });
+
+                const userToken = jwt.sign({ _id: newUser.id, email: newUser.email }, secret, { expiresIn: "1d" });
+                res.cookie("usertoken", userToken, {
+                    httpOnly: true
+                }).json({ message: "success", user: newUser });
+                // res.json({ message: "succes", user: newUser });
             }
-        } catch(err) {
+        } catch (err) {
             console.log(err);
             return res.status(400).json(err);
+        }
+    },
+    login: async (req, res) => {
+        try {
+            const user = await User.findOne({ email: req.body.email });
+            if (user) {
+                const passwordMatch = await bcrypt.compare(req.body.password, user.password);
+                if (passwordMatch) {
+                    const userToken = jwt.sign({ _id: user.id, email: user.email }, secret, { expiresIn: "1d" });
+                    res.cookie("usertoken", userToken, {
+                        httpOnly: true
+                    }).json({ message: "success", user: user });
+                }
+                else { //else for if passwords don't match
+                    res.status(400).json({ message: "Invalid login attempt" });
+                }
+            } else { //else there was no user with that email
+                res.status(400).json({ message: "Invalid login attempt" });
+            }
+        }
+        catch (err) {
+            console.log(err);
+            return res.status(400).json(err);
+        }
     }
-}
 }
 
 
--- server/routes/user.routes.js
+++ server/routes/user.routes.js
@@ -2,4 +2,5 @@ const UserController = require("../controllers/user.controller");
 
 module.exports = app => {
     app.post("/api/users/register", UserController.register);
+    app.post("/api/users/login", UserController.login);
 }
